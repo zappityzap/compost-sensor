79db9b1f978a9ae6b7170c4fb345d88fb45f04c7
zappityzap <zappityzap@proton.me>
2024-08-04 12:50:36 -0700

    dynamic device types publish data to mqtt, but don't appear as device types in HA

--- rx/rx.ino
+++ rx/rx.ino
@@ -3,6 +3,7 @@
 #include <RH_RF95.h>
 #include <ArduinoJson.h>
 #include <ArduinoHA.h>
+#include <map>
 
 #include "config.h"
 
@@ -22,11 +23,30 @@ WiFiClient client;
 HADevice device;
 HAMqtt mqtt(client, device, 40);
 HASensorNumber baseBattery("baseBattery", HASensorNumber::PrecisionP2);
-HASensorNumber sensorBattery("sensorBattery", HASensorNumber::PrecisionP2);
-HASensorNumber soilTemperature("soilTemperature", HASensorNumber::PrecisionP2);
-HASensorNumber airTemperature("airTemperature", HASensorNumber::PrecisionP2);
-HASensorNumber wetness("wetness", HASensorNumber::PrecisionP1);
-HASensorNumber loraRSSI("loraRSSI", HASensorNumber::PrecisionP1);
+
+std::map<String, float[NUM_READINGS]> loraRssiReadingsMap;
+std::map<String, int> loraRssiIndexMap;
+std::map<String, float> loraRssiAverageMap;
+
+std::map<String, HASensorNumber*> loraRssiMap;
+std::map<String, String*> loraRssiEntityMap;
+std::map<String, String*> loraRssiNameMap;
+
+std::map<String, HASensorNumber*> sensorBatteryMap;
+std::map<String, String*> sensorBatteryEntityMap;
+std::map<String, String*> sensorBatteryNameMap;
+
+std::map<String, HASensorNumber*> soilTemperatureMap;
+std::map<String, String*> soilTemperatureEntityMap;
+std::map<String, String*> soilTemperatureNameMap;
+
+std::map<String, HASensorNumber*> airTemperatureMap;
+std::map<String, String*> airTemperatureEntityMap;
+std::map<String, String*> airTemperatureNameMap;
+
+std::map<String, HASensorNumber*> wetnessMap;
+std::map<String, String*> wetnessEntityMap;
+std::map<String, String*> wetnessNameMap;
 
 String uniqueID = "";
 
@@ -100,30 +120,11 @@ void setup() {
   device.setSoftwareVersion("1.0.0");
   device.enableSharedAvailability();
   device.enableLastWill();
+  device.enableExtendedUniqueIds(); 
 
   baseBattery.setIcon("mdi:battery");
   baseBattery.setName("Base Battery");
   baseBattery.setUnitOfMeasurement("V");
-
-  loraRSSI.setIcon("mdi:antenna");
-  loraRSSI.setName("Sensor LoRa RSSI");
-  loraRSSI.setUnitOfMeasurement("dBm");
-
-  soilTemperature.setIcon("mdi:temperature-celsius");
-  soilTemperature.setName("Soil Temperature");
-  soilTemperature.setUnitOfMeasurement("C");
-
-  sensorBattery.setIcon("mdi:battery");
-  sensorBattery.setName("Sensor Battery");
-  sensorBattery.setUnitOfMeasurement("V");
-
-  airTemperature.setIcon("mdi:temperature-celsius");
-  airTemperature.setName("Air Temperature");
-  airTemperature.setUnitOfMeasurement("C");
-
-  wetness.setIcon("mdi:ski-water");
-  wetness.setName("Wetness");
-  wetness.setUnitOfMeasurement("Wetness");
 
   // mqtt.setKeepAlive(60); // set the keep alive interval to 60 seconds, default is 15 seconds
   mqtt.begin(MQTT_BROKER_ADDR, MQTT_BROKER_PORT, MQTT_USER, MQTT_PASS);
@@ -139,6 +140,7 @@ void loop() {
   static int index = 0;
   static float baseBatteryAverage = 0;
   static float loraRssiAverage = 0;
+  bool published = false;
 
   if (rf95.available()) {
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
@@ -156,19 +158,15 @@ void loop() {
       if (!initialized) {
         for (int i = 0; i < NUM_READINGS; i++) {
             baseBatteryReadings[i] = batteryVoltage;
-            loraRSSIReadings[i] = loraRSSIValue;
         }
         baseBatteryAverage = batteryVoltage;
-        loraRSSIAverage = loraRSSIValue;
         initialized = true;
       }
       baseBatteryReadings[index] = batteryVoltage;
-      loraRSSIReadings[index] = loraRSSIValue;
       index = (index + 1) % NUM_READINGS;
       baseBatteryAverage = calculateMovingAverage(baseBatteryReadings, NUM_READINGS);
-      loraRSSIAverage = calculateMovingAverage(loraRSSIReadings, NUM_READINGS);
-      baseBattery.setValue(baseBatteryAverage, true);
-      loraRSSI.setValue(loraRSSIAverage, true);
+      published = baseBattery.setValue(baseBatteryAverage);
+      Serial.println(published ? "Published baseBatteryAverage" : "Failed to publish baseBatteryAverage");
 
       buf[len] = '\0';
       Serial.print("JSON length: "); Serial.print(strlen((char *)buf));
@@ -183,10 +181,85 @@ void loop() {
       float airTemperatureValue = doc["air_temp"];
       float wetnessValue = doc["wetness"];
       
-      soilTemperature.setValue(soilTemperatureValue, true);
-      sensorBattery.setValue(sensorBatteryValue, true);
-      airTemperature.setValue(airTemperatureValue, true);
-      wetness.setValue(wetnessValue, true);
+      // Dynamically create sensors if they don't already exist
+      if (loraRssiMap.find(sensorId) == loraRssiMap.end()) {
+        loraRssiEntityMap[sensorId] = new String("S" + sensorId + "_loraRssi");
+        loraRssiNameMap[sensorId] = new String("S" + sensorId + " LoRa RSSI");
+        loraRssiMap[sensorId] = new HASensorNumber(loraRssiEntityMap[sensorId]->c_str(), HASensorNumber::PrecisionP1);
+        loraRssiMap[sensorId]->setIcon("mdi:antenna");
+        loraRssiMap[sensorId]->setName(loraRssiNameMap[sensorId]->c_str());
+        loraRssiMap[sensorId]->setUnitOfMeasurement("dBm");
+
+        // Initialize RSSI readings and index
+        for (int i = 0; i < NUM_READINGS; i++) {
+            loraRssiReadingsMap[sensorId][i] = 0;
+        }
+        loraRssiIndexMap[sensorId] = 0;
+        loraRssiAverageMap[sensorId] = 0;
+      }
+      if (soilTemperatureMap.find(sensorId) == soilTemperatureMap.end()) {
+        soilTemperatureEntityMap[sensorId] = new String("S" + sensorId + "_soilTemp");
+        soilTemperatureNameMap[sensorId] = new String("S" + sensorId + " Soil Temperature");
+        soilTemperatureMap[sensorId] = new HASensorNumber(soilTemperatureEntityMap[sensorId]->c_str(), HASensorNumber::PrecisionP2);
+        soilTemperatureMap[sensorId]->setIcon("mdi:temperature-celsius");
+        soilTemperatureMap[sensorId]->setName(soilTemperatureNameMap[sensorId]->c_str());
+        soilTemperatureMap[sensorId]->setUnitOfMeasurement("C");
+      }
+      if (sensorBatteryMap.find(sensorId) == sensorBatteryMap.end()) {
+        sensorBatteryEntityMap[sensorId] = new String("S" + sensorId + "_sensorBattery");
+        sensorBatteryNameMap[sensorId] = new String("S" + sensorId + " Battery");
+        sensorBatteryMap[sensorId] = new HASensorNumber(sensorBatteryEntityMap[sensorId]->c_str(), HASensorNumber::PrecisionP2);
+        sensorBatteryMap[sensorId]->setIcon("mdi:battery");
+        sensorBatteryMap[sensorId]->setName(sensorBatteryNameMap[sensorId]->c_str());
+        sensorBatteryMap[sensorId]->setUnitOfMeasurement("V");
+      }
+      if (airTemperatureMap.find(sensorId) == airTemperatureMap.end()) {
+        airTemperatureEntityMap[sensorId] = new String("S" + sensorId + "_airTemp");
+        airTemperatureNameMap[sensorId] = new String("S" + sensorId + " Air Temperature");
+        airTemperatureMap[sensorId] = new HASensorNumber(airTemperatureEntityMap[sensorId]->c_str(), HASensorNumber::PrecisionP2);
+        airTemperatureMap[sensorId]->setIcon("mdi:temperature-celsius");
+        airTemperatureMap[sensorId]->setName(airTemperatureNameMap[sensorId]->c_str());
+        airTemperatureMap[sensorId]->setUnitOfMeasurement("C");
+      }
+      if (wetnessMap.find(sensorId) == wetnessMap.end()) {
+        wetnessEntityMap[sensorId] = new String("S" + sensorId + "_wetness");
+        wetnessNameMap[sensorId] = new String("S" + sensorId + " Wetness");
+        wetnessMap[sensorId] = new HASensorNumber(wetnessEntityMap[sensorId]->c_str(), HASensorNumber::PrecisionP1);
+        wetnessMap[sensorId]->setIcon("mdi:ski-water");
+        wetnessMap[sensorId]->setName(wetnessNameMap[sensorId]->c_str());
+        wetnessMap[sensorId]->setUnitOfMeasurement("Wetness");
+      }
+
+      // Update RSSI readings
+      int loraRssiIndex = loraRssiIndexMap[sensorId];
+      loraRssiReadingsMap[sensorId][index] = loraRssiValue;
+      index = (index + 1) % NUM_READINGS;
+      loraRssiIndexMap[sensorId] = index;
+
+      // Calculate moving average
+      float loraRssiAverage = calculateMovingAverage(loraRssiReadingsMap[sensorId], NUM_READINGS);
+      loraRssiAverageMap[sensorId] = loraRssiAverage;
+
+      // Send to Home Assistant
+      published = loraRssiMap[sensorId]->setValue(loraRssiAverage);
+      Serial.print("loraRssi "); Serial.print(loraRssiMap[sensorId]->uniqueId());
+      Serial.println(published ? " Published" : " Failed to publish");
+
+      published = soilTemperatureMap[sensorId]->setValue(soilTemperatureValue);
+      Serial.print("soilTemperatureValue "); Serial.print(soilTemperatureMap[sensorId]->uniqueId());
+      Serial.println(published ? " Published" : " Failed to publish");
+
+      published = sensorBatteryMap[sensorId]->setValue(sensorBatteryValue);
+      Serial.print("sensorBatteryValue "); Serial.print(sensorBatteryMap[sensorId]->uniqueId());
+      Serial.println(published ? " Published" : " Failed to publish");
+
+      published = airTemperatureMap[sensorId]->setValue(airTemperatureValue);
+      Serial.print("airTemperatureValue "); Serial.print(airTemperatureMap[sensorId]->uniqueId());
+      Serial.println(published ? " Published" : " Failed to publish");
+
+      published = wetnessMap[sensorId]->setValue(wetnessValue);
+      Serial.print("wetnessValue "); Serial.print(wetnessMap[sensorId]->uniqueId());
+      Serial.println(published ? " Published" : " Failed to publish");
 
       Serial.print("RX ");
       Serial.print("ID: "); Serial.print(sensorId);
